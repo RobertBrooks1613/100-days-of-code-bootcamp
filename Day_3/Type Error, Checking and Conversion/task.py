650c058d9d485119c89ac32adc73af7d8c57b99f
Robert <Robertbrooks1613@outlook.com>
2024-11-17 11:31:08 -0500

    Delete Day_3 directory

--- Day_3/Type Error, Checking and Conversion/task.py
+++
@@ -1,6 +0,0 @@
-print("Number of letters in your name: " + str(len(input("Enter your name: "))) + "\n Thanks for using this program!")
-
-mylist = ["Bobby", 500, 500.5, True]
-
-for item in mylist:
-    print(type(item))
