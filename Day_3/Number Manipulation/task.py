7f8e19d97b36d95dbb9adbc5e7869676c7363c10
Robert <Robertbrooks1613@outlook.com>
2024-11-17 11:00:40 -0500

    Add files via upload

---
+++ Day_3/Number Manipulation/task.py
@@ -0,0 +1,10 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi))
+
+print(round(bmi))
+
+print((15 / 100) * 125)
+
+print(f"Your BMI is {bmi}")
---
+++ Day_3/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,6 @@
+print("Number of letters in your name: " + str(len(input("Enter your name: "))) + "\n Thanks for using this program!")
+
+mylist = ["Bobby", 500, 500.5, True]
+
+for item in mylist:
+    print(type(item))
